e22f24a353e903cb6b705e8c58b951e6a1273389
github-action-benchmark <github@users.noreply.github.com>
2022-09-25 23:34:04 +0000

    add PseudonymGeneratorBenchmarks (benchmarkdotnet) benchmark result for 0b50b22059070fa8b8b86609f7b4ceb25ca0a825

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664148383578,
+  "lastUpdate": 1664148844098,
   "repoUrl": "https://github.com/chgl/vfps",
   "entries": {
     "PseudonymGeneratorBenchmarks": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 1328.9973123990571,
             "unit": "ns",
             "range": "± 0.677253033784542"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chgl@users.noreply.github.com",
+            "name": "chgl",
+            "username": "chgl"
+          },
+          "committer": {
+            "email": "chgl@users.noreply.github.com",
+            "name": "chgl",
+            "username": "chgl"
+          },
+          "distinct": true,
+          "id": "0b50b22059070fa8b8b86609f7b4ceb25ca0a825",
+          "message": "ci: give benchmark job write access to repo",
+          "timestamp": "2022-09-26T01:24:51+02:00",
+          "tree_id": "13a0b67751241ee7a31974006a8f65710a9c966c",
+          "url": "https://github.com/chgl/vfps/commit/0b50b22059070fa8b8b86609f7b4ceb25ca0a825"
+        },
+        "date": 1664148842232,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Vfps.Benchmarks.PseudonymGeneratorBenchmarks.CryptoRandomBase64UrlEncodedGenerator",
+            "value": 2095.1275922684445,
+            "unit": "ns",
+            "range": "± 76.00669532686017"
+          },
+          {
+            "name": "Vfps.Benchmarks.PseudonymGeneratorBenchmarks.HexEncodedSha256HashGenerator",
+            "value": 1526.8360054202196,
+            "unit": "ns",
+            "range": "± 54.926338466222894"
           }
         ]
       }
