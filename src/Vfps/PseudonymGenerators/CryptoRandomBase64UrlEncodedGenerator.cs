5949e251e984c1882eb859834cd10b3f23c2b539
chgl <chgl@users.noreply.github.com>
2022-09-28 15:19:12 +0200

    refactor: made crypto pseudonym generation code simpler

--- src/Vfps/PseudonymGenerators/CryptoRandomBase64UrlEncodedGenerator.cs
+++ src/Vfps/PseudonymGenerators/CryptoRandomBase64UrlEncodedGenerator.cs
@@ -6,34 +6,26 @@ namespace Vfps.PseudonymGenerators;
 
 public class CryptoRandomBase64UrlEncodedGenerator : IPseudonymGenerator
 {
-    private readonly byte[] hashedMachineNameBytes;
+    private static readonly byte[] hashedMachineNameBytes = SHA256.HashData(Encoding.UTF8.GetBytes(Environment.MachineName));
 
     public CryptoRandomBase64UrlEncodedGenerator()
     {
-        hashedMachineNameBytes = SHA256.HashData(Encoding.UTF8.GetBytes(Environment.MachineName));
     }
 
     public string GeneratePseudonym(string originalValue, uint pseudonymLength)
     {
-        var ticks = BitConverter.GetBytes(Environment.TickCount64).AsSpan(3, 4);
+        var hashedMachineName = hashedMachineNameBytes.AsSpan(0, 4);
 
-        var hashedMachineNameSpan = hashedMachineNameBytes.AsSpan(0, 4);
+        var ticks = BitConverter.GetBytes(Environment.TickCount).AsSpan();
 
         var rand = RandomNumberGenerator.GetBytes((int)pseudonymLength).AsSpan();
 
-        byte[] buffer = new byte[hashedMachineNameSpan.Length + ticks.Length + rand.Length];
+        var buffer = new byte[hashedMachineName.Length + ticks.Length + rand.Length];
 
-        var span = new Span<byte>(buffer, 0, buffer.Length);
+        hashedMachineName.CopyTo(buffer);
+        ticks.CopyTo(buffer.AsSpan(hashedMachineName.Length));
+        rand.CopyTo(buffer.AsSpan(hashedMachineName.Length + ticks.Length));
 
-        int index = 0;
-        ticks.CopyTo(span.Slice(index, ticks.Length));
-        index += ticks.Length;
-
-        hashedMachineNameSpan.CopyTo(span.Slice(index, hashedMachineNameSpan.Length));
-        index += hashedMachineNameSpan.Length;
-
-        rand.CopyTo(span.Slice(index, rand.Length));
-
-        return WebEncoders.Base64UrlEncode(span);
+        return WebEncoders.Base64UrlEncode(buffer);
     }
 }
