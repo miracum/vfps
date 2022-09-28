81216fe979662762c0361881f3ce6388d290110f
chgl <chgl@users.noreply.github.com>
2022-09-28 18:11:41 +0200

    test: re-added intergation test for the migrations image (#9)
    not yet run as part of ci

---
+++ src/Vfps.IntegrationTests/NetworkFixture.cs
@@ -0,0 +1,23 @@
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Networks;
+
+namespace Vfps.IntegrationTests;
+
+public sealed class NetworkFixture : IAsyncLifetime
+{
+    public IDockerNetwork Network { get; } = new TestcontainersNetworkBuilder()
+        .WithDriver(NetworkDriver.Bridge)
+        .WithName(Guid.NewGuid().ToString("D"))
+        .Build();
+
+    public Task InitializeAsync()
+    {
+        return this.Network.CreateAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return this.Network.DeleteAsync();
+    }
+}
