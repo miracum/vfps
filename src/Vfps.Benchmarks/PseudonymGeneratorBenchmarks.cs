2f856daab200c78120f7550a9132787fb9a25820
chgl <chgl@users.noreply.github.com>
2023-06-28 22:32:55 +0200

    feat: added uuid4 pseudonym generator (#62)

--- src/Vfps.Benchmarks/PseudonymGeneratorBenchmarks.cs
+++ src/Vfps.Benchmarks/PseudonymGeneratorBenchmarks.cs
@@ -9,12 +9,16 @@ public class PseudonymGeneratorBenchmarks
 {
     private readonly CryptoRandomBase64UrlEncodedGenerator crbase64Generator = new();
     private readonly HexEncodedSha256HashGenerator sha256HashGenerator = new();
+    private readonly Uuid4Generator uuid4Generator = new();
 
     [Benchmark]
     public string CryptoRandomBase64UrlEncodedGenerator() =>
-        crbase64Generator.GeneratePseudonym("test", 16);
+        crbase64Generator.GeneratePseudonym("test", 64);
 
     [Benchmark]
     public string HexEncodedSha256HashGenerator() =>
         sha256HashGenerator.GeneratePseudonym("test", 64);
+
+    [Benchmark]
+    public string Uuid4HashGenerator() => uuid4Generator.GeneratePseudonym("test", 36);
 }
