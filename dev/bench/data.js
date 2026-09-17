3821951d1d15277ee51f9b0413a263727967dce5
github-action-benchmark <github@users.noreply.github.com>
2026-09-17 20:11:48 +0000

    add PseudonymGeneratorBenchmarks (benchmarkdotnet) benchmark result for ac51866acc07fee6d434420cee501b4c87b069e4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789675372932,
+  "lastUpdate": 1789675908668,
   "repoUrl": "https://github.com/miracum/vfps",
   "entries": {
     "PseudonymGeneratorBenchmarks": [
@@ -9474,6 +9474,66 @@ window.BENCHMARK_DATA = {
             "value": 1228.105737539438,
             "unit": "ns",
             "range": "± 1.5969119716913536"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "64022198+miracum-bot@users.noreply.github.com",
+            "name": "miracum-bot",
+            "username": "miracum-bot"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ac51866acc07fee6d434420cee501b4c87b069e4",
+          "message": "chore(master): release 1.16.4 (#330)",
+          "timestamp": "2026-09-17T22:08:55+02:00",
+          "tree_id": "e7719e539d17bbaf16f5c756fcc4fd8e78ecd0ea",
+          "url": "https://github.com/miracum/vfps/commit/ac51866acc07fee6d434420cee501b4c87b069e4"
+        },
+        "date": 1789675907929,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Vfps.Benchmarks.PseudonymGeneratorBenchmarks.CryptoRandomBase64UrlEncodedGenerator",
+            "value": 1214.465807233538,
+            "unit": "ns",
+            "range": "± 3.138275983534982"
+          },
+          {
+            "name": "Vfps.Benchmarks.PseudonymGeneratorBenchmarks.Uuid4Generator",
+            "value": 685.3181774956839,
+            "unit": "ns",
+            "range": "± 1.3019312049270444"
+          },
+          {
+            "name": "Vfps.Benchmarks.PseudonymGeneratorBenchmarks.Uuid7Generator",
+            "value": 713.303808927536,
+            "unit": "ns",
+            "range": "± 1.7847004375794915"
+          },
+          {
+            "name": "Vfps.Benchmarks.PseudonymGeneratorBenchmarks.FullRandomHexEncodedGenerator",
+            "value": 1137.7283816704382,
+            "unit": "ns",
+            "range": "± 1.7624718894038662"
+          },
+          {
+            "name": "Vfps.Benchmarks.PseudonymGeneratorBenchmarks.FullRandomBase62EncodedGenerator",
+            "value": 1272.6093616485596,
+            "unit": "ns",
+            "range": "± 0.9780962225511504"
+          },
+          {
+            "name": "Vfps.Benchmarks.PseudonymGeneratorBenchmarks.FullRandomBase32EncodedGenerator",
+            "value": 1224.1885111490885,
+            "unit": "ns",
+            "range": "± 1.897357770991738"
           }
         ]
       }
